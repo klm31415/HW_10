7069a99d9f5c6a4989fce4eca1e7c53019f51cdd
klm31415 <klm31415@gmail.com>
2023-10-15 21:12:03 +0300

    to do homework

--- src/phonebook/Records.java
+++ src/phonebook/Records.java
@@ -1,2 +1,33 @@
-package phonebook;public class Records {
+package phonebook;
+
+public class Records {
+    public String name;
+    public String phoneNumber;
+
+    public Records(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: "+getName() +
+                " Phone: " + getPhoneNumber();
+    }
 }
